c3b2e1a7d63fbbeb22b819bdd030b024d7da0d53
spothapragada <sriharsha.pothapragada@data-axle.com>
2021-02-20 23:03:24 -0800

    Added a Location class to pass to the router

--- fastapi-jetbrains-webcast/api/weather_api.py
+++ fastapi-jetbrains-webcast/api/weather_api.py
@@ -1,9 +1,11 @@
 from typing import Optional
 import fastapi
+from models.location import Location
+
 
 router = fastapi.APIRouter()
 
 
 @router.get('/api/umbrella')
-def do_i_need_an_umbrella(city: str, country: str = 'us', state: Optional[str] = None):
-    return {"city": city}
+def do_i_need_an_umbrella(location: Location = fastapi.Depends()):
+    return location
