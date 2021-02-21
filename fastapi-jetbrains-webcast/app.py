61ede9248c27f54447a02d5369ef15ff60c31110
spothapragada <sriharsha.pothapragada@data-axle.com>
2021-02-20 19:43:58 -0800

    Creating a webapp using FastAPI, tested some rudimentary functionality and logging capabilities

---
+++ fastapi-jetbrains-webcast/app.py
@@ -0,0 +1,26 @@
+import fastapi
+import uvicorn
+from views import home
+import logging
+
+logging.basicConfig(filename='log_file', level=logging.DEBUG, format="'%(asctime)s %(message)s'")
+api = fastapi.FastAPI()
+
+
+def configure():
+    api.include_router(home.router)
+
+
+# @api.get('/')
+# def index():
+#     return {
+#         "message": "Hello World!",
+#         "status": "OK"
+#     }
+
+# @api.get('/api/calculate')
+# def calculate():
+#     return 2 + 2
+configure()
+if __name__ == '__main__':
+    uvicorn.run(api, host='127.0.0.1', port=8000)
